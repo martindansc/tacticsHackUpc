2c73253612c25480de6376fd1ffbcd14c2203650
martin.dansc <martin.dansc@gmail.com>
2017-03-04 01:04:36 +0100

    Added base game

--- state.js
+++ state.js
@@ -11,5 +11,15 @@ var setUserInput = function(obj) {
 }
 
 var addNewPlayer = function(obj) {
+    var id = obj.id;
+
+    //create new player obj
+    var newPlayerObj = {id : id, };
+    state[id] = newPlayerObj;
     
+    //create user input obj
+    userInputObj[id] = {id : id, spell : '', numAttack : -1};
 }
+
+exports.setUserInput = setUserInput;
+exports.addNewPlayer = addNewPlayer;
