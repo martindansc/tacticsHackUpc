6103e3a59efb50fc0431735b02f283c16392b902
martin.dansc <martin.dansc@gmail.com>
2017-03-04 01:23:40 +0100

    set state

--- state.js
+++ state.js
@@ -1,20 +1,33 @@
-var state = {
+var info = require('./info');
 
-};
+var currentLocation = {};
 
-var userInputs = {
+var state = {};
 
-}
+var userInputs = {}
 
 var setUserInput = function(obj) {
 
 }
 
 var addNewPlayer = function(obj) {
+
+    //check that are less than 4 players
+    if(obj) return;
+
     var id = obj.id;
 
+    var playerType = newPlayerType();
+
     //create new player obj
-    var newPlayerObj = {id : id, };
+    var newPlayerObj = {
+        id : id, 
+        type : playerType, 
+        health : info[playerType].health, 
+        lv : 0, 
+        spells : info[playerType].spells, 
+        avaliableNums : [1,2,3,4,5]
+    };
     state[id] = newPlayerObj;
     
     //create user input obj
