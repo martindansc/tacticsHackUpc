aa46acd4a4584a639988e6221d8568fbd6c1d9dd
martin.dansc <martin.dansc@gmail.com>
2017-03-04 20:16:26 +0100

    fixed id bug

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,17 +5,18 @@ var myClass;
 //init
 var socket = io();
 
-socket.on('connection', function(){
-    myId = socket.io.engine.id;
+socket.on('connect', function() {
+  console.log('My id: ' + socket.io.engine.id);
+  myId = socket.io.engine.id;
 });
 
 socket.on('state', function(obj){
     console.log(obj);
 
-    var playerState = obj.state;
-    var globalState = obj.currentLocation;
+    var state = obj.state;
+    var location = obj.currentLocation;
 
-    $('#CharacterHeader').text("Character T");
+    $('#CharacterHeader').text("Character: " + state[myId].type);
 });
 
 //state
