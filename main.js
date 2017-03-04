25f4769a57e71e731a3907de0f5ff5cf69151474
Melani <melani_92_2@hotmail.com>
2017-03-04 20:23:45 +0100

    fixes

--- main.js
+++ main.js
@@ -40,5 +40,5 @@ io.on('connection', function(socket){
 
 	//Add connection
 	var newPlayer = state.addNewPlayer(id);
-
+	socket.emit('id', id);
 });
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,17 +5,20 @@ var myClass;
 //init
 var socket = io();
 
-socket.on('connection', function(){
-    myId = socket.io.engine.id;
+socket.on('id', function(id){
+    myId = id;
 });
 
 socket.on('state', function(obj){
+    console.log(myId);
     console.log(obj);
 
     var playerState = obj.state;
     var globalState = obj.currentLocation;
 
-    $('#CharacterHeader').text("Character T");
+    $('#CharacterHeader').text(playerState[myId].type);
+    //$('#Health').text("Health: " + state.id.)
+    //$('#Level')
 });
 
 //state
