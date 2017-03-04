911f35a1b5f0a85874d73092e97d8e3692eb14dc
adriaaap <adria_aap@hotmail.com>
2017-03-04 01:24:25 +0100

    set user input

--- state.js
+++ state.js
@@ -1,3 +1,9 @@
+
+var newPlayerType = function(){
+
+}
+
+
 var state = {
 
 };
@@ -7,7 +13,14 @@ var userInputs = {
 }
 
 var setUserInput = function(obj) {
-
+    var id = obj.id;
+    var attack = obj.attack;
+    if (attack % 1 === 0) { //attack is a card
+        userInputs[id].numAttack = attack;
+    }
+    else { //attack is spell
+        userInputs[id].spell = attack;
+    }
 }
 
 var addNewPlayer = function(obj) {
